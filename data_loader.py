a0156a3087181afb49c115be9adc883018ba6ba4
Ilya Trushkin <viciousde@gmail.com>
2019-11-13 01:52:17 +0300

    123

--- data_loader.py
+++ data_loader.py
@@ -20,10 +20,10 @@ test_loader = torch.utils.data.DataLoader(
                         transforms.ToTensor(),
                         transforms.Normalize((0.1307,), (0.3081,))
                     ])),
-    batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    batch_size=1000, shuffle=True, num_workers=1, pin_memory=True)
 
 test_df = pd.read_csv("test.csv")
 test_images = (test_df.iloc[:,:].values).astype('float32')
 test_images = test_images.reshape(test_images.shape[0], 28, 28)
 test_images_tensor = torch.tensor(test_images)/255.0
-kaggle_loader=torch.utils.data.DataLoader(test_images_tensor)
+kaggle_loader=torch.utils.data.DataLoader(test_images_tensor, batch_size=1000, num_workers=1, shuffle=False)
