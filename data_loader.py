2598d4f8367f32784fae73fa767289af27ecdf38
Ilya Trushkin <viciousde@gmail.com>
2019-11-13 01:33:57 +0300

    kkk

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import torch
 import torch.utils.data
 from torchvision import datasets, transforms
 import PIL
+import pandas as pd
 
 
 
@@ -20,7 +21,7 @@ test_loader = torch.utils.data.DataLoader(
                         transforms.Normalize((0.1307,), (0.3081,))
                     ])),
     batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
-    
+
 test_df = pd.read_csv("test.csv")
 test_images = (test_df.iloc[:,:].values).astype('float32')
 test_images = test_images.reshape(test_images.shape[0], 28, 28)
