0707e242dfa58a892e87e60a3cc0e709714dac56
Илья Трушкин <viciousde@gmail.com>
2019-11-14 02:27:27 +0300

    move to cpu

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def train(model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
             
-def test(model, device, test_loader, use_wandb):
+def test(model, device, test_loader, use_wandb, output_dir):
     model.eval()
     preds=torch.LongTensor().to(device)
     with torch.no_grad():
@@ -35,10 +35,10 @@ def test(model, device, test_loader, use_wandb):
             data = data.to(device)
             output = model(data)
             torch.cat((preds,output.argmax(dim=1, keepdim=True)))
-    results = pd.Series(np.array(preds, dtype = np.int32),name="Label")
+    results = pd.Series(np.array(preds.cpu(), dtype = np.int32),name="Label")
     submission = pd.concat([pd.Series(range(1,len(results)+1), dtype=np.int32, name = "ImageId"),results],axis = 1)
     submission = submission.astype(np.int32)
-    submission.to_csv("/content/gdrive/My Drive/12345/predictions.csv",index=False)
+    submission.to_csv(output_dir + "/predictions.csv", index=False)
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.4f}%)\n'.format(
@@ -68,11 +68,13 @@ if __name__ == "__main__":
     argparser.add_argument("--epochs", type=int, default=100)
     argparser.add_argument("--momentum", type=float, default=0.5)
     argparser.add_argument("--use-wandb", type=bool, default=False)
+    argparser.add_argument("--output-dir", type=str, default=".")
     args = argparser.parse_args()
     lr = args.lr
     epochs = args.epochs
     momentum = args.momentum
     use_wandb= args.use_wandb
+    output_dir=args.output_dir
     if use_wandb:
         wandb.init(project="digit-recognizer")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -85,7 +87,7 @@ if __name__ == "__main__":
 
     for epoch in range(1, epochs + 1):    
         train(model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader, use_wandb)
+        test(model, device, test_loader, use_wandb, output_dir)
 
     if use_wandb:
         torch.save(model.state_dict(), os.path.join(wandb.run.dir, 'model.pt'))
