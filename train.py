3e777345f577b070eba828701bebe030a1882595
Илья Трушкин <viciousde@gmail.com>
2019-11-14 02:37:35 +0300

    fix+formatting

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import wandb
 
 import argparse
 
+
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -26,44 +27,46 @@ def train(model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            
+
+
 def test(model, device, test_loader, use_wandb, output_dir):
     model.eval()
-    preds=torch.LongTensor().to(device)
+    preds = torch.LongTensor().to(device)
     with torch.no_grad():
         for _, data in enumerate(test_loader):
             data = data.to(device)
             output = model(data)
-            torch.cat((preds,output.argmax(dim=1, keepdim=True)))
-    results = pd.Series(np.array(preds.cpu(), dtype = np.int32),name="Label")
-    submission = pd.concat([pd.Series(range(1,len(results)+1), dtype=np.int32, name = "ImageId"),results],axis = 1)
+            torch.cat((preds, output.argmax(dim=1, keepdim=True)))
+    results = pd.Series(np.array(preds.cpu(), dtype=np.int32), name="Label")
+    submission = pd.concat([pd.Series(
+        range(1, len(results)+1), dtype=np.int32, name="ImageId"), results], axis=1)
     submission = submission.astype(np.int32)
     submission.to_csv(output_dir + "/predictions.csv", index=False)
-    test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.4f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
     if use_wandb:
-        wandb.log({"Test Accuracy": 100. * correct / len(test_loader.dataset), "Test Loss": test_loss})
+        wandb.log({"Test Accuracy": 100. * correct /
+                   len(test_loader.dataset), "Test Loss": test_loss})
 
-def make_predictions(model,data_loader,device):
+
+def make_predictions(model, data_loader, device):
     model.eval()
     test_preds = torch.LongTensor()
-    
+
     for data in data_loader:
         if torch.cuda.is_available():
             data = data.cuda()
-            
+
         output = model(data)
-        
+
         preds = output.cpu().data.max(1, keepdim=True)[1][0]
         test_preds = torch.cat((test_preds, preds), dim=0)
-        
+
     return test_preds
 
+
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(description="Trains the 15 LeNet ensemble.")
+    argparser = argparse.ArgumentParser(
+        description="Trains the 15 LeNet ensemble.")
     argparser.add_argument("--lr", type=float, default=0.01)
     argparser.add_argument("--epochs", type=int, default=100)
     argparser.add_argument("--momentum", type=float, default=0.5)
@@ -73,19 +76,21 @@ if __name__ == "__main__":
     lr = args.lr
     epochs = args.epochs
     momentum = args.momentum
-    use_wandb= args.use_wandb
-    output_dir=args.output_dir
+    use_wandb = args.use_wandb
+    output_dir = args.output_dir
     if use_wandb:
         wandb.init(project="digit-recognizer")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    train_loader = torch.utils.data.DataLoader(dataset=MNISTTrainDataset("train.csv"), batch_size=64, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=MNISTTestDataset("test.csv"),batch_size=64, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(
+        dataset=MNISTTrainDataset("train.csv"), batch_size=64, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        dataset=MNISTTestDataset("test.csv"), batch_size=64, shuffle=False)
     model = LeNetEnsemble(15, device)
     if use_wandb:
         wandb.watch(model, log="all")
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
-    for epoch in range(1, epochs + 1):    
+    for epoch in range(1, epochs + 1):
         train(model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader, use_wandb, output_dir)
 
